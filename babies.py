8e30a41ec6da0f856938dad80dcff33729c61fb1
Alexandra Orth <alo2117@columbia.edu>
2012-09-30 01:54:01 -0400

    HOMG Get is working

--- babies.py
+++ babies.py
@@ -9,6 +9,15 @@ def index():
 	spermcount =  search.searching("Justin Bieber")
 	return render_template('index.html')
 
+app.route('/',methods=['GET','POST'])
+def print_form():
+    if request.method == 'POST':
+        return render_template('index.html')
+    if request.method == 'GET':
+	    print render_template('index.html',result=request.form['spermcount'])
+	    return render_template('index.html',result=request.form['spermcount'])
+
+
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port=port)
