7da5630597cd40c75a7d021fbaa4629a06e19df5
Daria Jung <djj2115cu@gmail.edu>
2012-09-29 22:57:50 -0400

    added static folder

--- babies.py
+++ babies.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template
+import search
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-		return 'Hello World!'
+def index():
+	return render_template('index.html')
 
 if __name__ == '__main__':
-		app.run()
+		app.run(debug=True)
